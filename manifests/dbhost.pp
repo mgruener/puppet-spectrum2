c1df25bd27a79472f32b7c2ec6a100e82697ad25
Michael Gruener <michael.gruener@chaosmoon.net>
2014-07-20 15:27:44 +0200

    Use exported resources to create the databases

--- manifests/dbhost.pp
+++ manifests/dbhost.pp
@@ -1,20 +1,3 @@
-class spectrum2::dbhost (
-  $mysqlpassword,
-  $serverhostname = 'localhost',
-  $mysqluser      = 'spectrum2',
-  $mysqldb        = 'spectrum2',
-) {
-
-  if downcase($serverhostname) in downcase([ $::fqdn, $::hostname ]) {
-    $spectrum2server = 'localhost'
-  }
-  else {
-    $spectrum2server = $serverhostname
-  }
-
-  mysql::db { $mysqldb:
-    user     => $mysqluser,
-    password => $mysqlpassword,
-    host     => $spectrum2server
-  }
+class spectrum2::dbhost {
+  Mysql::Db <<| tag == 'spectrum2_transport_db' |>>
 }
